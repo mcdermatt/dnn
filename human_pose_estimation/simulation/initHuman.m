d494697012d560e677c02c37277d158f2733c159
Matt McDermott <41551029+mcdermatt@users.noreply.github.com>
2021-03-28 18:57:59 -0400

    starting project

---
+++ human_pose_estimation/simulation/initHuman.m
@@ -0,0 +1,22 @@
+j0pi = 0;
+j1pi = 0;
+j2pi = 0;
+
+j0vi = 0;
+j1vi = 0;
+j2vi = 0;
+
+fx = [0 15]; %+x -> across body
+fy = [3 -15];
+fz = [0 55]; %Force on hand forwards
+
+hipsll = -30;
+hipsul = 35;
+j0ll = -30;
+j0ul = 30;
+cwll = -90; %chicken wing lower limit
+cwul = 0;
+j1ll = -180;
+j1ul = 180;
+j2ll = -130;
+j2ul = 5;
