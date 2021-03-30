e47fa04f041bc2dc4dd665fa409ea85bbff04d70
Matt McDermott <41551029+mcdermatt@users.noreply.github.com>
2021-03-30 00:23:05 -0400

    fixed elbow, renamed all joints

--- human_pose_estimation/simulation/initHuman.m
+++ human_pose_estimation/simulation/initHuman.m
@@ -1,38 +1,36 @@
-%TODO: Rename joints
-%j0- hips x axis
-%j1- hips y axis
-%j2- hips z axis
-%j3- shoulder x axis
-%j4- shoulder y axis (chicken wing)
-%j5- shoulder z axis (butterfly)
-%j6- elbow
-
-j0pi = 0; %initial hips side(?)
-j1pi = 60; %60; %initial shoulder raise
-j2pi = -45; %-45; %initial elbow
+j0pi = 0; %spine twist
+j1pi = 0; % hips side
+j2pi = 0; %bend over
+j3pi = 0; %chicken wing
+j4pi = 20; %butterfly [-30 to 90]
+j5pi = 60; %curl
+j6pi = 30; %elbow
 
 j0vi = 0;
 j1vi = 0;
 j2vi = 0;
+j3vi = 0;
+j4vi = 0;
+j5vi = 0;
+j6vi = 0;
 
 %"from workspace" block -> external force and torque block
-%[time force]??
-% fx = [0 0]; %+x -> across body
 % fx = timeseries((0:5)',[0 1 2 3 4]);
 timevec = ((0:1000)/500)';
-fz = timeseries(0*sin(timevec),timevec);
+fz = timeseries(40*sin(timevec),timevec);
 fx = timeseries(0*cos(timevec/2),timevec);
-fy = timeseries(0*sin(timevec),timevec);
+fy = timeseries(10*sin(timevec),timevec);
 % fy = [0 0];
 % fx = [0 0]; %Force on hand forwards
 
+%TODO: Rename limits
 hipsll = -30;
 hipsul = 35;
 j0ll = -30;
 j0ul = 30;
 cwll = -90; %chicken wing lower limit
 cwul = 20;
-j1ll = -180;
-j1ul = 180;
-j2ll = 20;
-j2ul = 100;
+j5ll = -180;
+j5ul = 180;
+j6ll = 0;
+j6ul = 130;
