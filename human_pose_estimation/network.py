3a7e2caa55708fea53bc4cc17f14cce389e1a933
Matt McDermott <41551029+mcdermatt@users.noreply.github.com>
2021-04-02 10:46:58 -0400

    fixed data structure issue, working on error handling in simscape script

--- human_pose_estimation/network.py
+++ human_pose_estimation/network.py
@@ -9,14 +9,59 @@ def Net(**kwargs):
 
     #TODO -> take trajPts as input
     inputs = keras.Input(shape=(10,3))
-
     #TODO Try 2D conv with 1 input filter?
     X = keras.layers.BatchNormalization()(inputs)
-    X = tf.keras.layers.Conv1D(filters = 3, kernel_size = 3, activation='relu')(X)
+    X = tf.keras.layers.Conv1D(filters = 16, kernel_size = 3, activation='relu')(X)
+    skip = tf.identity(X)
+
+    X = tf.keras.layers.Conv1D(filters = 16, kernel_size = 3, activation='relu', padding = 'same')(X)
     X = keras.layers.BatchNormalization()(X)
-    X = tf.keras.layers.Conv1D(filters = 3, kernel_size = 3, activation='relu')(X)
 
+    X = tf.keras.layers.Conv1D(filters = 16, kernel_size = 3, activation='relu', padding = 'same')(X)
+    X = keras.layers.BatchNormalization()(X)
 
+    X = keras.layers.add([X,skip])
+    X = keras.layers.ReLU()(X)
+    
+    #Start repeating-------------
+
+    X = tf.keras.layers.Conv1D(filters = 32, kernel_size = 3, activation='relu', padding = 'same')(X)
+    X = keras.layers.BatchNormalization()(X)
+    skip = tf.identity(X)
+
+    X = tf.keras.layers.Conv1D(filters = 32, kernel_size = 3, activation='relu', padding = 'same')(X)
+    X = keras.layers.BatchNormalization()(X)
+
+    X = keras.layers.add([X,skip])
+    X = keras.layers.ReLU()(X)
+
+    #---
+
+    X = tf.keras.layers.Conv1D(filters = 64, kernel_size = 3, activation='relu', padding = 'same')(X)
+    X = keras.layers.BatchNormalization()(X)
+    skip = tf.identity(X)
+
+    X = tf.keras.layers.Conv1D(filters = 64, kernel_size = 3, activation='relu', padding = 'same')(X)
+    X = keras.layers.BatchNormalization()(X)
+
+    X = keras.layers.add([X,skip])
+    X = keras.layers.ReLU()(X)
+
+    #---
+
+    X = tf.keras.layers.Conv1D(filters = 128, kernel_size = 3, activation='relu', padding = 'same')(X)
+    X = keras.layers.BatchNormalization()(X)
+    skip = tf.identity(X)
+
+    X = tf.keras.layers.Conv1D(filters = 128, kernel_size = 3, activation='relu', padding = 'same')(X)
+    X = keras.layers.BatchNormalization()(X)
+
+    X = keras.layers.add([X,skip])
+    X = keras.layers.ReLU()(X)
+
+    #---
+
+    #End layers--------------------------------
     X = keras.layers.Flatten()(X)
     X = keras.layers.Dense(units = 64, activation = 'relu')(X) 
     X = keras.layers.BatchNormalization()(X)
@@ -27,7 +72,35 @@ def Net(**kwargs):
     output = keras.layers.Dense(units=7, activation = 'tanh')(X)
 
     #rescale output to the range of motion of each joint
-    output = output*tf.constant([10., 10., 5., 110.,120.,120.,130.]) + tf.constant([0., 0., 0., -90., -30., -30., 0.])
+    output = output*tf.constant([25., 30., 33.75, 55. , 60., 180., 65.]) + tf.constant([0., 0., 26.25, -35., 30., 0., -65.])
+    #forgot a single negative sign...
+
+    model = tf.keras.Model(inputs,output)
+
+    return model
+
+
+
+def Net2(**kwargs):
+
+    """
+    Test Network
+    """
+
+    inputs = keras.Input(shape=(10,3))
+    
+    X = keras.layers.Flatten()(X)
+    X = keras.layers.Dense(units = 64, activation = 'relu')(X) 
+    X = keras.layers.BatchNormalization()(X)
+    X = keras.layers.Dense(units = 64, activation = 'relu')(X) 
+    X = keras.layers.BatchNormalization()(X)
+    X = keras.layers.Dense(units = 64, activation = 'relu')(X) 
+    X = keras.layers.BatchNormalization()(X)
+    output = keras.layers.Dense(units=7, activation = 'tanh')(X)
+
+    #rescale output to the range of motion of each joint
+    output = output*tf.constant([25., 30., 33.75, 55. , 60., 180., 65.]) + tf.constant([0., 0., 26.25, -35., 30., 0., -65.])
+    #forgot a single negative sign...
 
     model = tf.keras.Model(inputs,output)
 
